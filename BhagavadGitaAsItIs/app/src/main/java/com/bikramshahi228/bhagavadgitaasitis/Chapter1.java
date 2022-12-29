97a6db47d9534713322873d452f9b2c380ed2c27
Bikram shahi <bikramshahi624@gmail.com>
2022-12-30 02:01:10 +0530

    first

---
+++ BhagavadGitaAsItIs/app/src/main/java/com/bikramshahi228/bhagavadgitaasitis/Chapter1.java
@@ -0,0 +1,41 @@
+package com.bikramshahi228.bhagavadgitaasitis;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.bikramshahi228.bhagavadgitaasitis.databinding.ActivityChapter1Binding;
+
+public class Chapter1 extends AppCompatActivity {
+
+    private ActivityChapter1Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityChapter1Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
